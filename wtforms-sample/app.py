0e374a58be36d2578849991bbfbd4482f3b6ad2b
GIFFHub <yusuke94ka2nakano@gmail.com>
2024-11-20 08:55:28 +0900

    2024/11/19

--- wtforms-sample/app.py
+++ wtforms-sample/app.py
@@ -9,8 +9,8 @@ from forms import UserInfoForm
 @app.route('/', methods=['GET', 'POST'])
 def show_enter():
     form = UserInfoForm(request.form)
-    if request.method == 'POST':
-        pass
+    if request.method == 'POST' and form.validate():
+        return render_template('result.html', form=form)
     return render_template('enter.html', form=form)
 
 
